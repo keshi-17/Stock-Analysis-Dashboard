0083baa7c9715c854ed03b0b9204cefddec3e15d
Keshihan Sivakumar <114302657+keshi-17@users.noreply.github.com>
2025-07-29 23:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -788,7 +788,7 @@ def limit_stock_selection(selected_stocks):
     return selected_stocks
 
 if __name__ == '__main__':
-    app.run_server(
+    app.run(
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 8050)),
         debug=False
