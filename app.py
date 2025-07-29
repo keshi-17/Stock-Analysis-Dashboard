c66ea5bc557d588a14c57574dfb6452e5c2c5f11
Keshihan Sivakumar <114302657+keshi-17@users.noreply.github.com>
2025-07-29 23:42:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -787,5 +787,6 @@ def limit_stock_selection(selected_stocks):
         return selected_stocks[:10]
     return selected_stocks
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
